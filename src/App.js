531a6243dd51895d7313bcf6e6ac51a60c2b06a5
Hafid ait oubouhou <aitoubouhouhafid@gmail.com>
2020-04-16 05:54:41 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './theme/css/App.css';
 import {Route, Switch} from 'react-router-dom';
 import Home from "./component/Home";
 import About from "./component/About";
